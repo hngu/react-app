53d35396eb228d79dae216ab283568315ea621ed
Huy Ngu <huyngu100@gmail.com>
2017-10-10 22:56:05 -0400

    pass in text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button />
+        <Button text={'hello'}/>
       </div>
     );
   }
