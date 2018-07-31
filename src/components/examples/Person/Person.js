ee923c1b6e90b378de45bb379509c17cc6df5ece
Huy <hngu@wordstream.com>
2018-07-31 08:20:50 -0400

    added css file for component

--- src/components/examples/Person/Person.js
+++ src/components/examples/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css'
 
 const Person = (props) => {
     return (
-        <div>
+        <div className="person">
             <p>
                 I'm {props.name} and I am {props.age} years old!
                 <button onClick={props.incrementAge}>Increment Age</button>
