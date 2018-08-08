08ad466d0c6f137f2b5e64e0d4322200af4e24c4
Huy <hngu@wordstream.com>
2018-08-08 09:22:40 -0400

    add lifecycle component

---
+++ src/components/examples/LifeCycle/LifeCycle.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+const LifeCycle extends Component {
+    constructor() {
+        super(props)
+        // here you setup state or do modern ES6 and set state
+        // as property in this component shown below:
+    }
+
+    state = {
+
+    }
+}
+
+export default LifeCycle
