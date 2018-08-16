7b7b81120a101bb57aac915c335a91f9030731d6
Huy <hngu@wordstream.com>
2018-08-16 09:00:25 -0400

    additional comment

--- src/components/examples/Persons/Persons.js
+++ src/components/examples/Persons/Persons.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Person from './Person/Person'
 
+// here no wrapping div: you can return arrays
+// as a matter of fact you can do something like:
+//return [
+//    <p key="1">Hi</p>
+//    <p key="2">World</p>
+//]
 const Persons = (props) => props.persons.map((person, index) => {
     return (
         <Person name={person.name}
