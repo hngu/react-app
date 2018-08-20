adaaf10c8923e798b03916627c36a2501bccc677
Huy <hngu@wordstream.com>
2018-08-20 08:00:48 -0400

    added more hocs

--- src/components/examples/Persons/Person/Person.js
+++ src/components/examples/Persons/Person/Person.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import classes from './Person.css'
+import React from 'react';
+import classes from './Person.css';
+import Aux from '../../../../hoc/examples/Aux/Aux';
+import withClass from '../../../../hoc/examples/withClass';
 
 const Person = (props) => {
     return (
-        <div className={classes['person']}>
+        <Aux>
             <div>
                 <p>I'm {props.name} and I am {props.age} years old!</p>
                 <p><input type="text" value={props.name} onChange={props.changeName} /></p>
                 <p><button onClick={props.incrementAge}>Increment Age</button></p>
             </div>
             <div>{props.children}</div>
-        </div>
+        </Aux>
     );
 }
 
-export default Person
+export default withClass(Person, classes['person']);
