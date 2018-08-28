a17a0881785501e3760fdd17be8a32ca0b11e980
Huy <hngu@wordstream.com>
2018-08-28 09:01:14 -0400

    set up burger app

---
+++ src/containers/BurgerApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BurgerApp extends Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default BurgerApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import BurgerApp from './containers/BurgerApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BurgerApp />, document.getElementById('root'));
 registerServiceWorker();
