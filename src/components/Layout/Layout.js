ca8b7a1c96e752eaf5e1154ae3154aaf5245e8ee
Huy <hngu@wordstream.com>
2018-08-30 08:57:38 -0400

    basic app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/examples/Aux/Aux';
+import classes from './Layout.css';
 
 const Layout = (props) => {
     return (
         <Aux>
             <div>Toolbar, Sidebar, Backdrop</div>
-            <main>
+            <main className={classes['Content']}>
                 {props.children}
             </main>
         </Aux>
--- src/containers/BurgerApp.js
+++ src/containers/BurgerApp.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout/Layout';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 class BurgerApp extends Component {
     render() {
         return (
             <Layout>
-                <p>Hello World!</p>
+                <BurgerBuilder />
             </Layout>
         );
     }
