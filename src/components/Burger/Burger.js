543f6f15d1d7d3557514523c9dfc665eaaa8cf63
Huy <hngu@wordstream.com>
2018-09-14 08:00:46 -0400

    zero ingredient state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map((ingredient) => {
+    let transformedIngredients = Object.keys(props.ingredients).map((ingredient) => {
         return [...Array(props.ingredients[ingredient])].map((_, index) => {
             return <BurgerIngredient key={`${ingredient}-${index}`} type={ingredient} />;
         });
+    }).reduce((arr, element) => {
+        return arr.concat(element);
     });
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients!</p>;
+    }
+
     return (
         <div className={classes['Burger']}>
             <BurgerIngredient type="bread-top" />
