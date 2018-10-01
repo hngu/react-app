d8111cad12fbc48f6dd2c4e118132ea6f9940747
Huy <hngu@wordstream.com>
2018-10-01 08:15:34 -0400

    display toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/examples/Aux/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => {
     return (
         <Aux>
-            <div>Toolbar, Sidebar, Backdrop</div>
+            <Toolbar />
             <main className={classes['Content']}>
                 {props.children}
             </main>
