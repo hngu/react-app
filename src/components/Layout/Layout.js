cc08865c3131ab782109047a854d6742122e6ebb
Huy <hngu@wordstream.com>
2018-10-05 08:44:02 -0400

    show sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/examples/Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 const Layout = (props) => {
     return (
         <Aux>
             <Toolbar />
+            <Sidedrawer />
             <main className={classes['Content']}>
                 {props.children}
             </main>
